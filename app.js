e51bcb183c9ea22429fa6b72b0014f73f8c88773
tobiasrother <trother93@gmail.com>
2019-05-28 11:48:09 +0200

    enable CORS

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
-var express = require("express");
-var app = express();
+var express = require("express")
+var cors = require("cors")
+var app = express()
 
 var data = require("./data")
 
-app.use(express.static('public'));
-app.use('/data', data);
+app.use(express.static('public'))
+app.use('*',cors())
+app.use('/data', data)
 
 app.listen(3000, function () {
-    console.log("Server running on port 3000");
+    console.log("Server running on port 3000")
 });
