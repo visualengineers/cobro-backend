95136d41d45231ba59e82bcf5e70d574bd88fbec
Dietrich Kammer <kammer@htw-dresden.de>
2019-06-05 14:48:33 +0200

    move server.js to root of repo

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const serverless = require('serverless-http');
 const app = express();
 const bodyParser = require('body-parser');
 
-var data = require("../data")
+var data = require("./data")
 
-app.use(express.static('../public'))
+app.use(express.static('public'))
 app.use('*',cors())
-app.use('./data', data)
+app.use('/data', data)
 
 app.use(bodyParser.json());
 app.use('/.netlify/functions/server', router);  // path must route to lambda
