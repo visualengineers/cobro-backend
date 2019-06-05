607ea2718caa66e3e6a5644d546ee453184d9785
Dietrich Kammer <kammer@htw-dresden.de>
2019-06-05 14:16:43 +0200

    add netlify scripts and config

---
+++ express/server.js
@@ -0,0 +1,17 @@
+'use strict';
+const express = require('express');
+const serverless = require('serverless-http');
+const app = express();
+const bodyParser = require('body-parser');
+
+var data = require("./data")
+
+app.use(express.static('public'))
+app.use('*',cors())
+app.use('/data', data)
+
+app.use(bodyParser.json());
+app.use('/.netlify/functions/server', router);  // path must route to lambda
+
+module.exports = app;
+module.exports.handler = serverless(app);
